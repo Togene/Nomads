f8745701e3013655b41508917d8eaf96946e3243
Eugene Martens <jhonnywhiplash@hotmail.com.au>
2019-11-16 02:24:16 +1000

    Added Initial Project

---
+++ index.php
@@ -0,0 +1,14 @@
+﻿<html>
+	<head>
+	<title>NOMADS</title>
+	<style>
+		body {margin:0;}
+		canvas{width:100%; height:100%;}
+	</style>
+	</head>
+	<body>
+		<script src = "js/libs/three.min.js"></script>
+		<script src = "js/nomads/nomads.js"></script>
+		<script src = "js/main.js"></script>
+	</body>
+</html>
---
+++ js/helpers/antlion.js
@@ -0,0 +1,1 @@
+/*used to grab all outside resources for the game*/
